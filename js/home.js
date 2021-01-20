37340637c2bf18f2dc80ca7c11d2c32f723930c0
Yurii <Yurri2002@gmail.com>
2021-01-20 21:00:27 +0200

    slider

--- js/home.js
+++ js/home.js
@@ -55,14 +55,14 @@ export default class Home{
         let index = 0;
 
         const activeSlide = n => {
-            for(slide of slides) {
+            for(let slide of slides) {
                 slide.classList.remove('active');
             }
             slides[n].classList.add('active');
         }
 
         const activeDot = n => {
-            for(dot of dots) {
+            for(let dot of dots) {
                 dot.classList.remove('active');
             }
             dots[n].classList.add('active');
